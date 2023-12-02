48ac03b05f5b5133e6cd6861685c2a144a2d540d
Ratika Swami <12ratikaswami@gmail.com>
2023-12-02 17:37:25 +0530

    project setup and api data fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { fetchData } from './api';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchDataFromApi = async () => {
+      try {
+        const apiData = await fetchData();
+        setData(apiData);
+        console.log(apiData)
+      } catch (error) {
+        console.error('Error in App component:', error);
+      }
+    };
+
+    fetchDataFromApi();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
